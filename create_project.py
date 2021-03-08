275735f7dd64b2247d2267ae4264387f53068254
Ben <bschreiber91@cs.huji.ac.il>
2021-03-08 19:30:59 +0200

    Add support for enterprise servers

--- create_project.py
+++ create_project.py
@@ -10,14 +10,16 @@ class ProjectCreator:
 
     def __init__(self, project_name: str):
         self.project_name: str = project_name
-        self.project_dir: str = ''
+        self.project_dir: str = read_json_attribute(Constants.JSON_PROJECT_DIR)
+        self.project_abs_path: str = os.path.join(self.project_dir, project_name)
         self.curr_path = Path(os.path.abspath(__file__))
+        self.server: str = ''
 
     def push_local_repo(self, remote_link: str):
         """
         Pushes the local repo to the remote
         """
-        os.chdir(self.project_dir)
+        os.chdir(self.project_abs_path)
         os.system(f'git remote add origin {remote_link} >> nul')
         os.system('git branch -M main >> nul')
         os.system('git push -u origin main >> nul')
@@ -27,17 +29,17 @@ class ProjectCreator:
         Goes to github, inits a repo with the project name, returns the link
         Throws a ValueError if a repo already exists with that name
         """
-        github_token = read_json_attribute(Constants.JSON_GITHUB_TOKEN)
-        g = Github(github_token)
+        github_token = read_json_attribute(Constants.GITHUB_CREDENTIALS)[self.server]
+        g = Github(login_or_token=github_token, base_url=f'https://{self.server}/api/v3')
         user = g.get_user()
-        repo = user.create_repo(self.project_name)
-        return f'https://github.com/{user.login}/{self.project_name}.git'
+        repo = user.create_repo(self.project_name, private=(self.server == Constants.HUJI_SERVER_CHOICE))
+        return f'https://{self.server}/{user.login}/{self.project_name}.git'
 
     def init_local_git(self):
         """
         Inits a local git repository, adds all files to it, and commits
         """
-        os.chdir(self.project_dir)
+        os.chdir(self.project_abs_path)
         os.system('git init >> nul')
         os.system('git add . >> nul')
         os.system('git commit -m "Initial Commit" >> nul')
@@ -49,16 +51,12 @@ class ProjectCreator:
         template_dir = os.path.join(self.curr_path.parent, Constants.TEMPLATE_DIR)
 
         gitignore_loc = os.path.join(template_dir, Constants.GITIGNORE_FILE)
-        gitignore_dest = os.path.join(self.project_dir, Constants.GITIGNORE_FILE)
+        gitignore_dest = os.path.join(self.project_abs_path, Constants.GITIGNORE_FILE)
         os.system(f'cp {gitignore_loc} {gitignore_dest}')
 
         readme_loc = os.path.join(template_dir, Constants.README_FILE)
-        readme_dest = os.path.join(self.project_dir, Constants.README_FILE)
+        readme_dest = os.path.join(self.project_abs_path, Constants.README_FILE)
         os.system(f'cp {readme_loc} {readme_dest}')
-
-    def set_project_dir(self, project_dir: str):
-        self.project_dir = project_dir
-        write_json_attribute(Constants.JSON_PROJECT_DIR, project_dir)
 
     def create_folder(self) -> bool:
         """
@@ -66,13 +64,10 @@ class ProjectCreator:
         Returns True iff it successfully creates a folder and cds into it.
         If a folder with the project_name already exists, then return False
         """
-        gparent_dir = self.curr_path.parent.parent
-        project_dir = os.path.join(gparent_dir, self.project_name)
-        if os.path.isdir(project_dir):
+        if os.path.isdir(self.project_abs_path):
             return False
-        self.set_project_dir(project_dir)
-        os.system(f'mkdir {project_dir}')
-        print(project_dir)
+        os.system(f'mkdir {self.project_abs_path}')
+        print(self.project_abs_path)
         return True
 
 
@@ -83,7 +78,14 @@ class ProjectCreator:
         self.init_local_git()
 
     def second_half(self):
-        self.project_dir = read_json_attribute(Constants.JSON_PROJECT_DIR)
+        print('Which server would you like? (Type the server)\n')
+        servers = read_json_attribute(Constants.GITHUB_CREDENTIALS).keys()
+        for i, _server in enumerate(servers):
+            print(f' ({i + 1}) {_server}')
+        choice = input()
+        if choice not in set(servers):
+            error_handling()
+        self.server = choice
         remote_link = self.init_remote_git()
         self.push_local_repo(remote_link)
 
@@ -98,6 +100,7 @@ def write_json_attribute(attribute_name, value):
         data = json.load(f)
 
     data[attribute_name] = value
+
     with open(Constants.CREDENTIALS_FILE, 'w') as f2:
         json.dump(data, f2)
 
